946c0b7dee225112ee375041f83765177fa769bd
AakashPanta <Kimyuri466@gmail.com>
2026-04-03 01:56:40 +0000

    make main output config-aware

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from src.version import __version__
+from src.config import load_config
 
 
 def main():
+    config = load_config()
+    env = config["APP_ENV"]
+    debug = config["APP_DEBUG"]
+
     print(f"Mr-Robot project initialized. Version: {__version__}")
+    print(f"Environment: {env}")
+    print(f"Debug: {debug}")
 
 
 if __name__ == "__main__":
